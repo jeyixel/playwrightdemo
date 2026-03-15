70803ccd425b53ded62cf266fde362fe1382187f
Gihan <mastergba2001@gmail.com>
2026-03-15 16:42:52 +0530

    finalize asserttions

--- tests/assertions.spec.js
+++ tests/assertions.spec.js
@@ -7,23 +7,23 @@ test('assertion', async({page})=>{
 
     await expect(page).toHaveTitle(/dummy-ui/);
 
-    // check element is present or not
+    
     await expect(page.getByRole('button', { name: 'Reveal Secret' })).toHaveCount(1)
 
-    // elemet handler
+   
     if(await page.$('text=Reveal Secret')){
         await page.locator('text=Reveal Secret').click()
     }
 
-    // check elemet is hiddle or visible
+    
     await expect(page.locator('text=Reveal Secret')).toBeVisible()
     await expect.soft(page.locator('text=Reveal Secret')).toBeHidden()
 
-    //check elemet enable or disable
+    
     await expect(page.locator('text=Reveal Secret')).toBeEnabled()
     await expect.soft(page.locator('text=Reveal Secret')).toBeDisabled()
 
-    // check text
+    
     await expect(page.locator('text=Quality Engineering Practice')).toHaveText('Quality Engineering Practice')
     await expect(page.locator('text=Quality Engineering Practice')).not.toHaveText('ABCD')
 
@@ -33,7 +33,7 @@ test('assertion', async({page})=>{
         await page.locator('text=Enter username').click()
     }
 
-    //visual validation with ss
+   
     await expect(page).toHaveScreenshot()
 
     await expect(page.getByRole('button', { name: 'Fetch API Data' })).toHaveCount(1)
@@ -41,6 +41,5 @@ test('assertion', async({page})=>{
      if(await page.$('text=Fetch API Data')){
         await page.locator('text=Fetch API Data').click()
     }
-
 
 });
